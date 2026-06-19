c19bb0cfa6ce989f2945d0ef0b5213879db4794b
elena33d <o.didenko@student.bachelor-goit.com.ua>
2026-06-19 11:27:02 +0000

    add pagination load more

--- js/render.js
+++ js/render.js
@@ -1,4 +1,4 @@
-export function renderBestsellers(items) {
+function renderBestsellers(items) {
   const list = document.querySelector(".bestsellers-list");
   list.innerHTML = "";
   items.forEach((item) => {
@@ -14,6 +14,27 @@ export function renderBestsellers(items) {
         <h3 class="bestsellers-name">${item.name}</h3>
         <p class="bestsellers-desc">${item.desc}</p>
         <p class="bestsellers-price">${item.price}</p>
+      </li>`
+    );
+  });
+}
+
+function renderBouquets(items) {
+  const list = document.querySelector(".bouquets-list");
+  list.innerHTML = "";
+  items.forEach((item) => {
+    list.insertAdjacentHTML(
+      "beforeend",
+      `<li class="bouquets-item">
+        <img
+          class="bouquets-image"
+          src="${item.image}"
+          srcset="${item.image} 1x, ${item.image2x} 2x"
+          alt="${item.alt}"
+        />
+        <h3 class="bouquets-name">${item.name}</h3>
+        <p class="bouquets-desc">${item.desc}</p>
+        <p class="bouquets-price">${item.price}</p>
       </li>`
     );
   });
