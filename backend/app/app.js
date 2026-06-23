390c3bc1a4b4fd2e64933d6d194d6c8270686e65
elena33d <o.didenko@student.bachelor-goit.com.ua>
2026-06-23 16:44:46 +0000

    commit

--- backend/app/app.js
+++ backend/app/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
+const path = require("path");
 
 const bouquetsRouter = require("../routes/bouquets");
 
@@ -9,6 +10,7 @@ const app = express();
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
+app.use("/photos", express.static(path.join(__dirname, "../public/photos")));
 
 app.use("/api/bouquets", bouquetsRouter);
 
