88f33dd0c80e23b9210897f690c3c279cc0c670e
elena33d <o.didenko@student.bachelor-goit.com.ua>
2026-06-27 11:50:41 +0000

    add swagger documentation

--- backend/app/app.js
+++ backend/app/app.js
@@ -2,6 +2,8 @@ const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 const path = require("path");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("../swagger.json");
 
 const bouquetsRouter = require("../routes/bouquets");
 
@@ -11,6 +13,7 @@ app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 app.use("/photos", express.static(path.join(__dirname, "../public/photos")));
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use("/api/bouquets", bouquetsRouter);
 
